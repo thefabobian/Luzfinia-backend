cf294ac2f2b6a959401673413b9472c93194b75b
Fabian Espinosa <fabianespinosapastrana@gmail.com>
2025-10-14 13:15:44 -0500

    agregué encriptacion en la password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,7 +12,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
     },
     password: {
       type: String,
@@ -19,6 +19,19 @@ const userSchema = new mongoose.Schema(
     },
   }
 );
+
+// encriptar la contraseña antes de guardar el usuario
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // si no se modificó, continuar
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Comparar contraseña
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;

04922ce1b2e6b78b0d24775d7618d08d1ac29836
Fabian Espinosa <fabianespinosapastrana@gmail.com>
2025-10-19 00:35:53 -0500

    modelo para la lecturas

---
+++ src/models/readingModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const readingSchema = new mongoose.Schema({
+    house: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "House",
+        required: true,
+    },
+    ts: { 
+        type: Date, 
+        required: true 
+    },
+    kwh: { // lectura incremental en khw
+        type: Number, 
+        required: true 
+    }, 
+    totalKwh: { // lectura total en kwh
+        type: Number,
+        required: true
+    },
+    activeAppliances: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "HouseAppliance",
+        },
+    ],
+});
+
+const Reading = mongoose.model("Reading", readingSchema);
+export default Reading;
